7a06ab6125539b789ac85b1d6fd53a17bd7013fa
Stuti Kesarwani <115646191+stutikesu@users.noreply.github.com>
2024-11-17 02:34:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -71,4 +71,5 @@ def generate_bio():
 if __name__ == '__main__':
     # Use the port assigned by Render or default to 5000 for local testing
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=False)
+
