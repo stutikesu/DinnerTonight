07dc36e1e6ff24308912324e3b126023b1ec826e
Stuti Kesarwani <115646191+stutikesu@users.noreply.github.com>
2024-11-17 03:51:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 import torch
-import os
 import random
 
 app = Flask(__name__)
@@ -63,6 +62,5 @@ def generate_bio():
     return render_template('result.html', bio=bio)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(debug=True)
 
